f7626e24762de20653d6c1d06d95f41c00755da2
DARSHAN-THE-CODER <reachdarshanv@gmail.com>
2023-04-22 13:35:43 +0530

    api url fix

--- client/constants/api.js
+++ client/constants/api.js
@@ -1,2 +1,2 @@
 // export const APIURL = "http://localhost:8000/api/v1"
-export const APIURL = "http://3.83.134.103/api/v1"
+export const APIURL = "http://ec2-18-212-177-34.compute-1.amazonaws.com/api/v1"
